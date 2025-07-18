a444a1c20ea1792e2b456712344439c58b1edbb8
Fahad Iqbal Shah <fahadiqbalshah41@gmail.com>
2025-07-18 11:25:34 +0530

    Requests in Backend

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// TYPES OF REQUESTS IN BACKEND
+
+//Get request
+app.get("/", (req, res) => {
+  res.send("This is a GET Request");
+});
+
+app.post("/items", (req, res) => {
+  res.send("This is a POST Request");
+});
+
+app.put("/items/:id", (req, res) => {
+  res.send("This is a PUT request");
+});
+
+app.delete("/items/:id", (req, res) => {
+  res.send("This is a DELETE request");
+});
 
 app.listen(port, () => {
-  console.log(`This Application is listening on port ${port}`)
-})
+  console.log(`This Application is listening on port ${port}`);
+});
