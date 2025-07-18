cd44ec5f01f3335a4030d9d1a27a9936b9872d06
Fahad Iqbal Shah <fahadiqbalshah41@gmail.com>
2025-07-18 19:21:55 +0530

    Using express and middlewares

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+import express from "express";
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+const app = express(); // Create an instance of express
 
-// TYPES OF REQUESTS IN BACKEND
+const PORT = process.env.PORT || 4000; // Set the port to either the environment variable PORT or 4000
+// Start the server and listen on the specified port
 
-//Get request
+// This is a Global Middleware and gets applied to all routes handeled by the app
+app.use(express.json()); // 🔑 This line is required to parse JSON body
+
 app.get("/", (req, res) => {
-  // res.send("This is a GET Request");
-  res.sendFile("/routing/dummy.html", { root: __dirname });
+  res.send("This is the main server and is a GET call"); // Responds to GET requests at the root URL
+});
+app.get("/hello", (req, res) => {
+  res.status(200).json({ Message: "This is the HELLO route of the server" }); // Responds to GET requests at /hello with a JSON object
 });
 
-app.post("/items", (req, res) => {
-  // res.send("This is a POST Request");
-  res.json({ name: "FAHAD", age: 24, city: "BANGALORE" });
+app.get("/health", (req, res) => {
+  console.log(req.query);
+
+  res.send("The server is HEALTHY"); // Responds to GET requests at /health with a health status message
 });
 
-app.put("/items/:id", (req, res) => {
-  res.send("This is a PUT request");
+app.post("/api/users", (req, res) => {
+  console.log("Name:", req.body.name); // Logs the name from the request body
+
+  res.json({ Message: "Recieved the name from the request body" });
 });
 
-app.delete("/items/:id", (req, res) => {
-  res.send("This is a DELETE request");
-});
-
-app.listen(port, () => {
-  console.log(`This Application is listening on port ${port}`);
-});
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`)); // This will log a message to the console when the server starts
